a4d07eca5f346567e2ba6e798ff116cbe0c9e40a
Elizabeth <student@elizabeths-computer.local>
2016-12-08 10:24:56 -0600

    Initial Commit

---
+++ bmiCalculatorFinal/ImageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ImageViewController.swift
+//  bmiCalculatorFinal
+//
+//  Created by Elizabeth on 12/6/16.
+//  Copyright © 2016 Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: ViewController
+
+{
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var healthLabel: UILabel!
+    
+    var bmiTwo = Double()
+    override func viewDidLoad()
+    {
+    var bmiString = String(format: "%.1f", bmiTwo)
+
+    bmiLabel.text = "Your BMI is \(bmiString)."
+    if bmiTwo < 18.5
+    {
+    healthLabel.text = "You are underweight."
+     view.backgroundColor = UIColor.red
+    }
+    else if bmiTwo >= 18.5 && bmiTwo <= 24.9
+    {
+    healthLabel.text = "Your weight is normal."
+    view.backgroundColor = UIColor.green
+    }
+    else if bmiTwo >= 25.0
+    {
+    healthLabel.text = "Your are overweight."
+    view.backgroundColor = UIColor.red
+    }
+}
+}
--- bmiCalculatorFinal/ViewController.swift
+++ bmiCalculatorFinal/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var inchesTextBox: UITextField!
+    @IBOutlet weak var feetTextBox: UITextField!
+    @IBOutlet weak var weightTextBox: UITextField!
+    var bmi = 0.0
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func calculateButton(_ sender: Any)
+    {
+        var weight = Double (weightTextBox.text!)
+        
+        var feet = Double(feetTextBox.text!)
+        
+        var inches = Double(inchesTextBox.text!)
+        
+        var kilograms = weight! * 0.45
+        
+        var feetToInches = feet! * 12
+        
+        var totalInches = feetToInches + inches!
+        
+        var finalHeight = totalInches * 0.025
+        
+         bmi = kilograms / (finalHeight * finalHeight)
+        print(bmi)
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let nextVC = segue.destination as! ImageViewController
+        nextVC.bmiTwo = bmi
+        
     }
 
 
 }
-
